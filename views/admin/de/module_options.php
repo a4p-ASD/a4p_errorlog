53dd7c40c2959d20e7dbdb34fe612372f782169b
a4p ASD <asd@apps4print.com>
2015-03-20 16:05:17 +0100

    falsche sprachvariable

--- views/admin/de/module_options.php
+++ views/admin/de/module_options.php
@@ -26,7 +26,7 @@ $aLang = array(
 	
 	'charset'									=> 'UTF-8',
 	
-	'SHOP_MODULE_GROUP_a4p_main'				=> 'apps4print Einstellungen',
-	'SHOP_MODULE_a4p_errorlog_logfile_abs'		=> 'Log-Datei',
-	'HELP_SHOP_MODULE_a4p_errorlog_logfile_abs'	=> 'bitte absoluten Pfad inklusive Dateiname angeben ( z.B.: /var/log/httpd/error_log oder /var/www/vhosts/_a4p_apache_logs/oxid-develop/error_log )',
+	'SHOP_MODULE_GROUP_a4p_main'					=> 'apps4print Einstellungen',
+	'SHOP_MODULE_a4p_errorlog__logfile_abs'			=> 'Log-Datei',
+	'HELP_SHOP_MODULE_a4p_errorlog__logfile_abs'	=> 'bitte absoluten Pfad inklusive Dateiname angeben ( z.B.: /var/log/httpd/error_log oder /var/www/vhosts/_a4p_apache_logs/oxid-develop/error_log )',
 );
